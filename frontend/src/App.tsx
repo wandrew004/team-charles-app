9964b5bd4443744cad12b34df665a435e4977e0c
angiechang <killmenow107329@gmail.com>
2025-03-23 20:10:15 -0400

    minimal_front_end_requested_changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import RecipePage from './pages/RecipePage';
-import CreateRecipePage from './pages/CreateRecipePage';
+// import CreateRecipePage from './pages/CreateRecipePage';
+import RecipeForm from './RecipeForm';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/recipe/:id" element={<RecipePage />} /> 
-        <Route path="/create" element={<CreateRecipePage />} />
-      </Routes>
-    </Router>
+<Routes>
+  <Route path="/" element={<HomePage />} />
+  <Route path="/recipe/:id" element={<RecipePage />} />
+  <Route path="/create" element={<RecipeForm />} />
+</Routes>
   );
 }
 
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,22 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import RecipeForm from './RecipeForm.tsx'
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+// import RecipeForm from './RecipeForm.tsx'
+import App from './App'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={new QueryClient()}>
-      <RecipeForm />
+      <App />
     </QueryClientProvider>
   </StrictMode>,
 )
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
