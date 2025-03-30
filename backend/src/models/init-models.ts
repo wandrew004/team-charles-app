cb4e1842dccc861213bdf196e1d0b7a4ce70ee8d
rmdluo <rmdluo@gmail.com>
2025-03-30 17:51:55 -0400

    make it singularize the model names

--- backend/src/models/init-models.ts
+++ backend/src/models/init-models.ts
@@ -1,88 +1,88 @@
 import type { Sequelize } from "sequelize";
 import { databaseversion as _databaseversion } from "./databaseversion";
 import type { databaseversionAttributes, databaseversionCreationAttributes } from "./databaseversion";
-import { ingredients as _ingredients } from "./ingredients";
-import type { ingredientsAttributes, ingredientsCreationAttributes } from "./ingredients";
-import { ownedingredients as _ownedingredients } from "./ownedingredients";
-import type { ownedingredientsAttributes, ownedingredientsCreationAttributes } from "./ownedingredients";
-import { recipeingredients as _recipeingredients } from "./recipeingredients";
-import type { recipeingredientsAttributes, recipeingredientsCreationAttributes } from "./recipeingredients";
-import { recipes as _recipes } from "./recipes";
-import type { recipesAttributes, recipesCreationAttributes } from "./recipes";
-import { recipesteps as _recipesteps } from "./recipesteps";
-import type { recipestepsAttributes, recipestepsCreationAttributes } from "./recipesteps";
-import { steps as _steps } from "./steps";
-import type { stepsAttributes, stepsCreationAttributes } from "./steps";
-import { units as _units } from "./units";
-import type { unitsAttributes, unitsCreationAttributes } from "./units";
+import { ingredient as _ingredient } from "./ingredient";
+import type { ingredientAttributes, ingredientCreationAttributes } from "./ingredient";
+import { ownedingredient as _ownedingredient } from "./ownedingredient";
+import type { ownedingredientAttributes, ownedingredientCreationAttributes } from "./ownedingredient";
+import { recipeingredient as _recipeingredient } from "./recipeingredient";
+import type { recipeingredientAttributes, recipeingredientCreationAttributes } from "./recipeingredient";
+import { recipe as _recipe } from "./recipe";
+import type { recipeAttributes, recipeCreationAttributes } from "./recipe";
+import { recipestep as _recipestep } from "./recipestep";
+import type { recipestepAttributes, recipestepCreationAttributes } from "./recipestep";
+import { step as _step } from "./step";
+import type { stepAttributes, stepCreationAttributes } from "./step";
+import { unit as _unit } from "./unit";
+import type { unitAttributes, unitCreationAttributes } from "./unit";
 
 export {
   _databaseversion as databaseversion,
-  _ingredients as ingredients,
-  _ownedingredients as ownedingredients,
-  _recipeingredients as recipeingredients,
-  _recipes as recipes,
-  _recipesteps as recipesteps,
-  _steps as steps,
-  _units as units,
+  _ingredient as ingredient,
+  _ownedingredient as ownedingredient,
+  _recipeingredient as recipeingredient,
+  _recipe as recipe,
+  _recipestep as recipestep,
+  _step as step,
+  _unit as unit,
 };
 
 export type {
   databaseversionAttributes,
   databaseversionCreationAttributes,
-  ingredientsAttributes,
-  ingredientsCreationAttributes,
-  ownedingredientsAttributes,
-  ownedingredientsCreationAttributes,
-  recipeingredientsAttributes,
-  recipeingredientsCreationAttributes,
-  recipesAttributes,
-  recipesCreationAttributes,
-  recipestepsAttributes,
-  recipestepsCreationAttributes,
-  stepsAttributes,
-  stepsCreationAttributes,
-  unitsAttributes,
-  unitsCreationAttributes,
+  ingredientAttributes,
+  ingredientCreationAttributes,
+  ownedingredientAttributes,
+  ownedingredientCreationAttributes,
+  recipeingredientAttributes,
+  recipeingredientCreationAttributes,
+  recipeAttributes,
+  recipeCreationAttributes,
+  recipestepAttributes,
+  recipestepCreationAttributes,
+  stepAttributes,
+  stepCreationAttributes,
+  unitAttributes,
+  unitCreationAttributes,
 };
 
 export function initModels(sequelize: Sequelize) {
   const databaseversion = _databaseversion.initModel(sequelize);
-  const ingredients = _ingredients.initModel(sequelize);
-  const ownedingredients = _ownedingredients.initModel(sequelize);
-  const recipeingredients = _recipeingredients.initModel(sequelize);
-  const recipes = _recipes.initModel(sequelize);
-  const recipesteps = _recipesteps.initModel(sequelize);
-  const steps = _steps.initModel(sequelize);
-  const units = _units.initModel(sequelize);
+  const ingredient = _ingredient.initModel(sequelize);
+  const ownedingredient = _ownedingredient.initModel(sequelize);
+  const recipeingredient = _recipeingredient.initModel(sequelize);
+  const recipe = _recipe.initModel(sequelize);
+  const recipestep = _recipestep.initModel(sequelize);
+  const step = _step.initModel(sequelize);
+  const unit = _unit.initModel(sequelize);
 
-  ingredients.belongsToMany(recipes, { as: 'recipeid_recipes', through: recipeingredients, foreignKey: "ingredientid", otherKey: "recipeid" });
-  recipes.belongsToMany(ingredients, { as: 'ingredientid_ingredients', through: recipeingredients, foreignKey: "recipeid", otherKey: "ingredientid" });
-  recipes.belongsToMany(steps, { as: 'stepid_steps', through: recipesteps, foreignKey: "recipeid", otherKey: "stepid" });
-  steps.belongsToMany(recipes, { as: 'recipeid_recipes_recipesteps', through: recipesteps, foreignKey: "stepid", otherKey: "recipeid" });
-  ownedingredients.belongsTo(ingredients, { as: "ingredient", foreignKey: "ingredientid"});
-  ingredients.hasOne(ownedingredients, { as: "ownedingredient", foreignKey: "ingredientid"});
-  recipeingredients.belongsTo(ingredients, { as: "ingredient", foreignKey: "ingredientid"});
-  ingredients.hasMany(recipeingredients, { as: "recipeingredients", foreignKey: "ingredientid"});
-  recipeingredients.belongsTo(recipes, { as: "recipe", foreignKey: "recipeid"});
-  recipes.hasMany(recipeingredients, { as: "recipeingredients", foreignKey: "recipeid"});
-  recipesteps.belongsTo(recipes, { as: "recipe", foreignKey: "recipeid"});
-  recipes.hasMany(recipesteps, { as: "recipesteps", foreignKey: "recipeid"});
-  recipesteps.belongsTo(steps, { as: "step", foreignKey: "stepid"});
-  steps.hasMany(recipesteps, { as: "recipesteps", foreignKey: "stepid"});
-  ingredients.belongsTo(units, { as: "standard_unit_unit", foreignKey: "standard_unit"});
-  units.hasMany(ingredients, { as: "ingredients", foreignKey: "standard_unit"});
-  recipeingredients.belongsTo(units, { as: "unit", foreignKey: "unitid"});
-  units.hasMany(recipeingredients, { as: "recipeingredients", foreignKey: "unitid"});
+  ingredient.belongsToMany(recipe, { as: 'recipeid_recipes', through: recipeingredient, foreignKey: "ingredientid", otherKey: "recipeid" });
+  recipe.belongsToMany(ingredient, { as: 'ingredientid_ingredients', through: recipeingredient, foreignKey: "recipeid", otherKey: "ingredientid" });
+  recipe.belongsToMany(step, { as: 'stepid_steps', through: recipestep, foreignKey: "recipeid", otherKey: "stepid" });
+  step.belongsToMany(recipe, { as: 'recipeid_recipes_recipesteps', through: recipestep, foreignKey: "stepid", otherKey: "recipeid" });
+  ownedingredient.belongsTo(ingredient, { as: "ingredient", foreignKey: "ingredientid"});
+  ingredient.hasOne(ownedingredient, { as: "ownedingredient", foreignKey: "ingredientid"});
+  recipeingredient.belongsTo(ingredient, { as: "ingredient", foreignKey: "ingredientid"});
+  ingredient.hasMany(recipeingredient, { as: "recipeingredients", foreignKey: "ingredientid"});
+  recipeingredient.belongsTo(recipe, { as: "recipe", foreignKey: "recipeid"});
+  recipe.hasMany(recipeingredient, { as: "recipeingredients", foreignKey: "recipeid"});
+  recipestep.belongsTo(recipe, { as: "recipe", foreignKey: "recipeid"});
+  recipe.hasMany(recipestep, { as: "recipesteps", foreignKey: "recipeid"});
+  recipestep.belongsTo(step, { as: "step", foreignKey: "stepid"});
+  step.hasMany(recipestep, { as: "recipesteps", foreignKey: "stepid"});
+  ingredient.belongsTo(unit, { as: "standard_unit_unit", foreignKey: "standard_unit"});
+  unit.hasMany(ingredient, { as: "ingredients", foreignKey: "standard_unit"});
+  recipeingredient.belongsTo(unit, { as: "unit", foreignKey: "unitid"});
+  unit.hasMany(recipeingredient, { as: "recipeingredients", foreignKey: "unitid"});
 
   return {
     databaseversion: databaseversion,
-    ingredients: ingredients,
-    ownedingredients: ownedingredients,
-    recipeingredients: recipeingredients,
-    recipes: recipes,
-    recipesteps: recipesteps,
-    steps: steps,
-    units: units,
+    ingredient: ingredient,
+    ownedingredient: ownedingredient,
+    recipeingredient: recipeingredient,
+    recipe: recipe,
+    recipestep: recipestep,
+    step: step,
+    unit: unit,
   };
 }
