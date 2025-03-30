46270837eaf5700a5d1df42e2bc246221e101e47
rmdluo <rmdluo@gmail.com>
2025-03-29 22:49:44 -0400

    add rollback script

---
+++ backend/db_setup_scripts/rollback_v0.2.sql
@@ -0,0 +1,20 @@
+BEGIN;
+
+-- 1. Drop OwnedIngredients table
+DROP TABLE IF EXISTS ownedingredients;
+
+-- 2. Remove unit_id column from RecipeIngredients
+ALTER TABLE recipeingredients
+    DROP CONSTRAINT IF EXISTS fk_recipeingredients_unit,
+    DROP COLUMN IF EXISTS unit_id;
+
+-- 3. Remove standard_unit and density from Ingredients
+ALTER TABLE ingredients
+    DROP CONSTRAINT IF EXISTS fk_ingredients_standard_unit,
+    DROP COLUMN IF EXISTS standard_unit,
+    DROP COLUMN IF EXISTS density;
+
+-- 4. Drop Units table
+DROP TABLE IF EXISTS units;
+
+COMMIT;
