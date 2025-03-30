d027d6035d7725ed65d24c804f9db5a9521bb865
rmdluo <rmdluo@gmail.com>
2025-03-29 23:12:42 -0400

    add migrating the units back in the rollback script

--- backend/db_setup_scripts/rollback_v0.2.sql
+++ backend/db_setup_scripts/rollback_v0.2.sql
@@ -1,20 +1,30 @@
 BEGIN;
 
--- 1. Drop OwnedIngredients table
-DROP TABLE IF EXISTS ownedingredients;
+-- 1. Recreate unit column in RecipeIngredients (before dropping the Units table)
+ALTER TABLE recipeingredients
+    ADD COLUMN unit TEXT;
 
--- 2. Remove unit_id column from RecipeIngredients
+-- 2. Restore unit text values from Units table via unitID
+UPDATE recipeingredients ri
+SET unit = u.name
+FROM units u
+WHERE ri.unitID = u.id;
+
+-- 3. Remove unitID column from RecipeIngredients
 ALTER TABLE recipeingredients
     DROP CONSTRAINT IF EXISTS fk_recipeingredients_unit,
     DROP COLUMN IF EXISTS unitID;
 
--- 3. Remove standard_unit and density from Ingredients
+-- 4. Remove standard_unit and density from Ingredients
 ALTER TABLE ingredients
     DROP CONSTRAINT IF EXISTS fk_ingredients_standard_unit,
     DROP COLUMN IF EXISTS standard_unit,
     DROP COLUMN IF EXISTS density;
 
--- 4. Drop Units table
+-- 5. Drop OwnedIngredients table
+DROP TABLE IF EXISTS ownedingredients;
+
+-- 6. Drop Units table
 DROP TABLE IF EXISTS units;
 
 COMMIT;
