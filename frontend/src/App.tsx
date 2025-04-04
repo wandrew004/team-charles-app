6f8180bc8ae457deea52bbc20ff6c797882d57e8
Albert Lin <albert1104@gmail.com>
2025-04-04 17:02:31 -0400

    added populate branch command

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,7 +14,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/recipe/:id" element={<RecipePage />} />
+        <Route path="/recipes/:id" element={<RecipePage />} />
         <Route path="/create" element={<RecipeForm />} />
         <Route path="/recipes" element={<Recipes />} />
         <Route path="/aggregate" element={<AggregationPage />} />
