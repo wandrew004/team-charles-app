33269f71e772a89e982da18ea432daf0d6d589f1
rmdluo <rmdluo@gmail.com>
2025-04-15 19:52:13 -0400

    add v0.0.3 db update/rollback scripts

---
+++ backend/db_setup_scripts/rollback/0.0.3.sql
@@ -0,0 +1,20 @@
+BEGIN;
+
+-- 1. Remove foreign key and column from 'owned_ingredients'
+ALTER TABLE owned_ingredients
+DROP CONSTRAINT IF EXISTS fk_owned_ingredients_user,
+DROP COLUMN IF EXISTS user_id;
+
+-- 2. Remove foreign key and column from 'recipes'
+ALTER TABLE recipes
+DROP CONSTRAINT IF EXISTS fk_recipes_user,
+DROP COLUMN IF EXISTS user_id;
+
+-- 3. Drop 'users' table
+DROP TABLE IF EXISTS users;
+
+-- 4. Remove 0.0.3 from the DatabaseVersion Table, fails if something depends on it
+DELETE FROM DatabaseVersion
+WHERE Version = '0.0.3';
+
+COMMIT;
